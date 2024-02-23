d5814df3358049f9357d9bdf66f1ffcbe2bbd87b
Anunoy Dussa <adussa3@gmail.com>
2024-02-23 15:52:33 -0800

    Updated App Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
+import { useState } from "react";
+import Header from "./components/Header";
+import Result from "./components/Result";
+import UserInput from "./components/UserInput";
+
+const INPUT_NAMES = ["initial-investment", "annual-investment", "expected-return", "duration"];
+
 function App() {
-  return (
-    <h1>React Investment Calculator</h1>
-  )
+    const [inputValues, setInputValues] = useState(() => {
+        const initialValues = {};
+        for (const name of INPUT_NAMES) {
+            initialValues[name] = "0";
+        }
+        return initialValues;
+    });
+
+    const handleInputChange = (event, inputName) => {
+        setInputValues((prevInputValues) => ({ ...prevInputValues, [inputName]: event.target.value }));
+    };
+
+    return (
+        <>
+            <Header />
+
+            <div id="user-input" className="input-group">
+                {INPUT_NAMES.map((name) => {
+                    return <UserInput key={name} inputName={name} inputValues={inputValues} onChangeInput={handleInputChange} />;
+                })}
+            </div>
+
+            <Result inputValues={inputValues} />
+        </>
+    );
 }
 
-export default App
+export default App;
